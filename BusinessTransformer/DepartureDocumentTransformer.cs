b98644ff7ee9f1e0b5c6864c1f380e306fbb4300
eliott <eliott.jaquier@gmail.com>
2024-12-07 14:09:32 +0100

    feat: add structure from class diagram

---
+++ BusinessTransformer/DepartureDocumentTransformer.cs
@@ -0,0 +1,16 @@
+﻿using BusinessTransformer.Records;
+using CommonInterfaces.DocumentsRelated;
+
+namespace BusinessTransformer;
+
+/// <summary>
+/// A class that transforms a DeparturesDocument into a TrainStation.
+/// </summary>
+public class DepartureDocumentTransformer : IDocumentTransformer<DeparturesDocument, TrainStation>
+{
+    /// <inheritdoc/>
+    public TrainStation Transform(DeparturesDocument input)
+    {
+        throw new NotImplementedException();
+    }
+}
