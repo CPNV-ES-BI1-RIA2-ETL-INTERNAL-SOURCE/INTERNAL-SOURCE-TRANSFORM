11da4d123f6fd38d75bb78ee18c119aae4377da4
eliott <eliott.jaquier@gmail.com>
2024-12-08 17:29:07 +0100

    refactor: based on CFF, L attribute can be null

--- BusinessTransformer/Records/Train.cs
+++ BusinessTransformer/Records/Train.cs
@@ -4,5 +4,5 @@ namespace BusinessTransformer.Records;
 /// Represents a train with associated identifiers.
 /// </summary>
 /// <param name="G">Group / category of the train.</param>
-/// <param name="L">Line the train operates on.</param>
-public record Train(string G, string L);
+/// <param name="L">Line the train operates on. Can be null if not specified.</param>
+public record Train(string G, string? L);
