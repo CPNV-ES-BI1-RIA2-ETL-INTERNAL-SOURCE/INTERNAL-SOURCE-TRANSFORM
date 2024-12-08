ecdd7f51d59c7b46506bf0723cbcf20f5b1089c8
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-08 10:10:27 +0100

    refactor: remove Signs from the commonInterfaces since they are not used in the PDFs

--- CommonInterfaces/Records/DocumentsRelated/Sign.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CommonInterfaces.DocumentsRelated;
-
-/// <summary>
-/// A structure that represents a sign (special character / logo) with its description.
-/// </summary>
-/// <param name="SignId">The unique identifier of the sign. (Ex. S, 1)</param>
-/// <param name="SignDescription">The description of the sign. (Ex. RER, VELOS: Réservation obligatoire)</param>
-public record Sign(string SignId, string SignDescription);
