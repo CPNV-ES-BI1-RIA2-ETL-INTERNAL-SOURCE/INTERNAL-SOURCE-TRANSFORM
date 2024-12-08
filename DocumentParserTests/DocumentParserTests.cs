f52b83137c5217e1f4c79727be1e28a56c76e167
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-08 11:31:12 +0100

    test: add tests on the revive method from DocumentParser

---
+++ DocumentParserTests/DocumentParserTests.cs
@@ -0,0 +1,77 @@
+namespace DocumentParserTests;
+
+public class DocumentParserTests
+{   
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test]
+    public void Revive_SingleInt()
+    {
+        // Given 
+        string rawDocument = "{\"content\": {\"1\": 45}}";
+
+        // When
+        var parsedDocument = DocumentParser.revive(rawDocument);
+        
+        // Then
+        Assert.AreEqual(45, parsedDocument["1"]);
+    }
+    
+    [Test]
+    public void Revive_SingleFloat()
+    {
+        // Given 
+        string rawDocument = "{\"content\": {\"1\": 45.5}}";
+
+        // When
+        var parsedDocument = DocumentParser.revive(rawDocument);
+        
+        // Then
+        Assert.AreEqual(45.5, parsedDocument["1"]);
+    }
+
+
+    [Test]
+    public void Revive_SingleString()
+    {
+        // Given 
+        string rawDocument = "{\"content\": {\"1\": \"Gare de Yverdon-les-Bains\"}}";
+
+        // When
+        var parsedDocument = DocumentParser.revive(rawDocument);
+        
+        // Then
+        Assert.AreEqual("Gare de Yverdon-les-Bains", parsedDocument["1"]);
+    }
+    
+    [Test]
+    public void Revive_ListOfStrings()
+    {
+        // Given 
+        var rawDocument = "{\"content\": {\"1\": [\"Gare de Yverdon-les-Bains\", \"Gare de Lausanne\"]}}";
+        
+        // When
+        var parsedDocument = DocumentParser.revive(rawDocument);
+        
+        // Then
+        Assert.AreEqual(new List<string> {"Gare de Yverdon-les-Bains", "Gare de Lausanne"}, parsedDocument["1"]);
+    }
+    
+    [Test]
+    public void Revive_ComplexDocument()
+    {
+        // Given 
+        var rawDocument = "{\"content\": {\"1\": [\"Gare de Yverdon-les-Bains\", \"Gare de Lausanne\"], \"2\": 83, \"3\": 4.5}}";
+        
+        // When
+        var parsedDocument = DocumentParser.revive(rawDocument);
+        
+        // Then
+        Assert.AreEqual(new List<string> {"Gare de Yverdon-les-Bains", "Gare de Lausanne"}, parsedDocument["1"]);
+        Assert.AreEqual(83, parsedDocument["2"]);
+        Assert.AreEqual(4.5, parsedDocument["3"]);
+    }
+}
