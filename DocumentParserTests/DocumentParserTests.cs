0e7133207f5a3db5573478130c82e5849271ffed
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-09 22:39:45 +0100

    test: write new tests on the Parse method from the DocumentParser

--- DocumentParserTests/DocumentParserTests.cs
+++ DocumentParserTests/DocumentParserTests.cs
@@ -4,4 +4,44 @@ namespace DocumentParserTests;
 
 public class DocumentParserTests
 {  
+    [Test]
+    public void Revive_SingleString()
+    {
+        // Given 
+        string rawDocument = "Gare de Yverdon-les-Bains";
+
+        // When
+        var parsedDocument = DocParser.Parse(rawDocument);
+
+        // Then
+        Assert.That(parsedDocument, Is.EqualTo("[\"Gare de Yverdon-les-Bains\"]"));
+    }
+    
+    [Test]
+    public void Revive_HeadersWithoutValues()
+    {
+        // Given 
+        string rawDocument = "Heure de départ        Ligne    Destination         Vias                                              Voie\n";
+
+        // When
+        var parsedDocument = DocParser.Parse(rawDocument);
+
+        // Then
+        Assert.That(parsedDocument, Is.EqualTo("[]"));
+    }
+    
+    [Test]
+    public void Revive_HeadersWithValues()
+    {
+        // Given 
+        string rawDocument = " Heure de départ        Ligne    Destination         Vias                                              Voie\n" +
+                             " 8 00                   IC 5     Lausanne                                                              2\n" +
+                             " 16 45                  IC 5     Genève Aéroport     Morges                                            2";
+
+        // When
+        var parsedDocument = DocParser.Parse(rawDocument);
+
+        // Then
+        Assert.That(parsedDocument, Is.EqualTo("[[{\"Heure de départ\": \"8 00\", \"Ligne\": \"IC 5\", \"Destination\": \"Lausanne\", \"Vias\": \"\", \"Voie\": \"2\"}, {\"Heure de départ\": \"16 45\", \"Ligne\": \"IC 5\", \"Destination\": \"Genève Aéroport\", \"Vias\": \"Morges\", \"Voie\": \"2\"}]]"));
+    }
 }
