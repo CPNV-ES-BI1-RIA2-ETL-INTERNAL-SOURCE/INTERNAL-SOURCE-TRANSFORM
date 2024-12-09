356db3e386bf7356cba5e27d3f5a9252a93fa183
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-09 22:24:38 +0100

    refactor: rebase the DocumentParser methods on the new interface

--- DocumentParser/DocumentParser.cs
+++ DocumentParser/DocumentParser.cs
@@ -1,47 +1,16 @@
 ﻿using System.Text.Json;
+using DeparturesDocument = CommonInterfaces.DocumentsRelated.DeparturesDocument;
 
 namespace DocumentParser;
-
 public class DocumentParser
 {
-    public static Dictionary<string, object> Parse(string rawDocument)
+    public static string Parse(string rawDocument)
     {
         throw new NotImplementedException();
     }
 
-    public static Dictionary<string, object> Revive(string rawDocument)
+    public static DeparturesDocument Revive(string rawDocument)
     {
-        var parsedDocument = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, JsonElement>>>(rawDocument);
-        if (parsedDocument == null || !parsedDocument.ContainsKey("content"))
-        {
-            throw new ArgumentException("Invalid document structure");
-        }
-
-        var content = parsedDocument["content"];
-        return CreateDictionary(content);
-    }
-
-    private static Dictionary<string, object> CreateDictionary(Dictionary<string, JsonElement> json)
-    {
-        return json.ToDictionary(
-            kvp => kvp.Key,
-            kvp => ConvertValue(kvp.Value) ?? kvp.Value.ToString()
-        );
-    }
-
-    private static object? ConvertValue(JsonElement element)
-    {
-        return element.ValueKind switch
-        {
-            JsonValueKind.Object => element.EnumerateObject().ToDictionary(property => property.Name, property => ConvertValue(property.Value)),
-            JsonValueKind.Array => element.EnumerateArray().Select(ConvertValue).ToList(),
-            JsonValueKind.String => element.GetString(),
-            JsonValueKind.Number when element.TryGetInt32(out var intValue) => intValue,
-            JsonValueKind.Number => element.GetDouble(),
-            JsonValueKind.True => true,
-            JsonValueKind.False => false,
-            JsonValueKind.Null => null,
-            _ => element.ToString()
-        };
+        throw new NotImplementedException();
     }
 }
