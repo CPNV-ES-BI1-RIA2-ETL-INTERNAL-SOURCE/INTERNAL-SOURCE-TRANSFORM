6247436b1c0f502b6ec46c8ab0ddc318ae55132e
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-09 22:41:34 +0100

    test: add a test for the complete document in DocumentParser

--- DocumentParserTests/DocumentParserTests.cs
+++ DocumentParserTests/DocumentParserTests.cs
@@ -44,4 +44,23 @@ public class DocumentParserTests
         // Then
         Assert.That(parsedDocument, Is.EqualTo("[[{\"Heure de départ\": \"8 00\", \"Ligne\": \"IC 5\", \"Destination\": \"Lausanne\", \"Vias\": \"\", \"Voie\": \"2\"}, {\"Heure de départ\": \"16 45\", \"Ligne\": \"IC 5\", \"Destination\": \"Genève Aéroport\", \"Vias\": \"Morges\", \"Voie\": \"2\"}]]"));
     }
+    
+    [Test]
+    public void Revive_CompleteDocument()
+    {
+        // Given 
+        string rawDocument = "Gare de Yverdon-les-Bains" +
+                             " Heure de départ        Ligne    Destination         Vias                                              Voie\n" +
+                             " 8 00                   IC 5     Lausanne                                                              2\n" +
+                             " 16 45                  IC 5     Genève Aéroport     Morges                                            2\n" +
+                             " \n" +
+                             " \n" +
+                             "Départ pour le 9 décembre 2024\n";
+
+        // When
+        var parsedDocument = DocParser.Parse(rawDocument);
+
+        // Then
+        Assert.That(parsedDocument, Is.EqualTo("[\"Gare de Yverdon-les-Bains\",[{\"Heure de départ\": \"8 00\", \"Ligne\": \"IC 5\", \"Destination\": \"Lausanne\", \"Vias\": \"\", \"Voie\": \"2\"}, {\"Heure de départ\": \"16 45\", \"Ligne\": \"IC 5\", \"Destination\": \"Genève Aéroport\", \"Vias\": \"Morges\", \"Voie\": \"2\"}], \"Départ pour le 9 décembre 2024\"]"));
+    }
 }
