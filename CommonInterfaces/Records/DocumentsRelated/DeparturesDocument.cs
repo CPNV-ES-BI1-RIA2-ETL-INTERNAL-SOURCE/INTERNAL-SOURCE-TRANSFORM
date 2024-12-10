015a02973926ede84ac4baf0fd97c0868ef1b107
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-10 23:41:50 +0100

    fix: replace type and typo from the common interfaces

--- CommonInterfaces/Records/DocumentsRelated/DeparturesDocument.cs
+++ CommonInterfaces/Records/DocumentsRelated/DeparturesDocument.cs
@@ -6,4 +6,4 @@
 /// <param name="RelatedTrainStationName">The train station name that the document is related to (all departures are from this station).</param>
 /// <param name="Date">The date from which the departures are listed.</param>
 /// <param name="Departure">A list of departures</param>
-public record DeparturesDocument(string RelatedTrainStationName, string Date, List<Departure> Departure);
+public record DeparturesDocument(string RelatedTrainStationName, string Date, List<Departure> Departures);
