d5f07cdebf75ffce9ea640fae0525c4568521586
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-10 21:26:34 +0100

    feat: add base DeparturesDocumentReviver class

---
+++ DocumentParser/DeparturesDocumentReviver.cs
@@ -0,0 +1,10 @@
+﻿using CommonInterfaces.DocumentsRelated;
+
+namespace DocumentParser;
+
+public class DeparturesDocumentReviver: IDocumentReviver<DeparturesDocument>
+{
+    public DeparturesDocument Revive(string rawDocument) {
+        throw new NotImplementedException();
+    }
+}
