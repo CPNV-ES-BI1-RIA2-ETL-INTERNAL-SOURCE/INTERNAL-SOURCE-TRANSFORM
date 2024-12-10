199694b5dfdfe13dddbc753a74ddb95cf0e6d697
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-10 23:35:13 +0100

    test: fix broken 'when' and assertions on DepartureDocumentReviverTests

--- DocumentParserTests/DeparturesDocumentReviverTests.cs
+++ DocumentParserTests/DeparturesDocumentReviverTests.cs
@@ -1,6 +1,6 @@
 using CommonInterfaces.DocumentsRelated;
 using DocumentParser;
-
+using System.Text.Json;
 namespace DocumentParserTests;
 
 public class DeparturesDocumentReviverTests
@@ -18,10 +18,7 @@ public class DeparturesDocumentReviverTests
     {
         // Given 
         string rawDocument = "Gare de Yverdon-les-Bains";
-
-        // When
-        var RevivedDocument = _documentReviver.Revive(rawDocument);
-
+        
         // Then
         Assert.Throws<FormatException>(() => _documentReviver.Revive(rawDocument));
     }
@@ -42,11 +39,12 @@ public class DeparturesDocumentReviverTests
         var RevivedDocument = _documentReviver.Revive(rawDocument);
 
         // Then
-        Assert.That(RevivedDocument, Is.EqualTo(
-            new DeparturesDocument("Gare de Yverdon-les-Bains", "9 décembre 2024", new List<Departure> {
-                new Departure("Lausanne", new List<string>(), 8, "IC 5", "2", null, new List<string>()),
-                new Departure("Genève Aéroport", new List<string> { "Morges" }, 16, "IC 5", "2", null, new List<string>())
-            })
-        ));
+        var departuresDocument = new DeparturesDocument("Gare de Yverdon-les-Bains", "Départ pour le 9 décembre 2024", new List<Departure>
+        {
+            new Departure("Lausanne", new List<string>(), "8 00", "IC 5", "2"),
+            new Departure("Genève Aéroport", new List<string> { "Morges" }, "16 45", "IC 5", "2")
+        });
+        
+        Assert.That(JsonSerializer.Serialize(RevivedDocument), Is.EqualTo(JsonSerializer.Serialize(departuresDocument)));
     }
 }
