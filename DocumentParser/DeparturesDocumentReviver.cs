455e2ac5066ee3cf70b9e6be14f22c7f7c223c57
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-10 23:39:56 +0100

    feat: add the methods to revive a rawDocument in the DepartureDocumentReviver

--- DocumentParser/DeparturesDocumentReviver.cs
+++ DocumentParser/DeparturesDocumentReviver.cs
@@ -1,10 +1,48 @@
-﻿using CommonInterfaces.DocumentsRelated;
+﻿using System.Text.Json.Nodes;
+using CommonInterfaces.DocumentsRelated;
 
 namespace DocumentParser;
 
 public class DeparturesDocumentReviver: IDocumentReviver<DeparturesDocument>
 {
     public DeparturesDocument Revive(string rawDocument) {
-        throw new NotImplementedException();
+        // Instantiate a json document from the raw
+        var jsonDocument = new DocumentParser().Parse(rawDocument);
+        var jsonArray = JsonNode.Parse(jsonDocument).AsArray();
+        
+        CheckFormat(jsonArray);
+        
+        var Departures = ReviveDepartures(jsonArray[1].AsArray());
+        return new DeparturesDocument(jsonArray[0].ToString(), jsonArray[2].ToString(), Departures);
+    }
+    
+    private void CheckFormat(JsonArray jsonArray) {
+        // Check that the json is in the correct business format
+        if (jsonArray.Count != 3 ||
+            !(jsonArray[0] is JsonValue) ||
+            !(jsonArray[1] is JsonArray) ||
+            !(jsonArray[2] is JsonValue)
+        ){
+            throw new FormatException();
+        }
+    }
+    
+    private List<Departure> ReviveDepartures(JsonArray jsonArray) {
+        var Departures = new List<Departure>();
+        
+        foreach (var departure in jsonArray) {
+            var departureObject = departure.AsObject();
+            var viasString = departureObject["Vias"].ToString();
+
+            Departures.Add(new Departure(
+                departureObject["Destination"].ToString(),
+                string.IsNullOrEmpty(viasString) ? new List<string>() : viasString.Split(", ").ToList(),
+                departureObject["Heure de d\u00E9part"].ToString(),
+                departureObject["Ligne"].ToString(),
+                departureObject["Voie"].ToString()
+            ));
+        }
+        
+        return Departures;
     }
 }
