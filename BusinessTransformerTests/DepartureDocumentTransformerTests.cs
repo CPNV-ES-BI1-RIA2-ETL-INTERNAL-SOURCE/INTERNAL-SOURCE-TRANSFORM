e6a184fca03506e4846a37873b6a198139a75b37
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-12-11 11:21:01 +0100

    fix: try to force culture info to see if it's a ci problem

--- BusinessTransformerTests/DepartureDocumentTransformerTests.cs
+++ BusinessTransformerTests/DepartureDocumentTransformerTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using BusinessTransformer;
 using BusinessTransformer.Records;
 using CommonInterfaces.DocumentsRelated;
@@ -14,6 +15,8 @@ namespace BusinessTransformerTests
         public void Setup()
         {
             _transformer = new DepartureDocumentTransformer();
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("fr-FR");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("fr-FR");
         }
 
         [Test]
