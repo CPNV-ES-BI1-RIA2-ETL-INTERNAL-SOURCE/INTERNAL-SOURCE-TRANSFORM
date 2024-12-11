5e7cdc55e0381e53fa069e0ff885bbe3921688bf
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-11 09:36:50 +0100

    refactor: enforce the format checker on DeparuresDocumentReviver

--- DocumentParser/DeparturesDocumentReviver.cs
+++ DocumentParser/DeparturesDocumentReviver.cs
@@ -21,10 +21,20 @@ public class DeparturesDocumentReviver: IDocumentReviver<DeparturesDocument>
         if (jsonArray.Count != 3 ||
             !(jsonArray[0] is JsonValue) ||
             !(jsonArray[1] is JsonArray) ||
-            !(jsonArray[2] is JsonValue)
+            !(jsonArray[2] is JsonValue) ||
+            !DeparturePropertyExists(jsonArray[1], "Destination")  ||
+            !DeparturePropertyExists(jsonArray[1], "Vias")  ||
+            !DeparturePropertyExists(jsonArray[1], "Heure de d\u00E9part")  ||
+            !DeparturePropertyExists(jsonArray[1], "Ligne")  ||
+            !DeparturePropertyExists(jsonArray[1], "Voie")
         ){
             throw new FormatException();
         }
+    }
+
+    private bool DeparturePropertyExists(JsonNode jsonNode, string propertyName)
+    {
+        return jsonNode[0].AsObject().ContainsKey(propertyName);
     }
     
     private List<Departure> ReviveDepartures(JsonArray jsonArray) {
