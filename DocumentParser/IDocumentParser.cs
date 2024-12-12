e090f5dd1d9df917ab3c17ccd9629030d18fc78d
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-12 11:24:54 +0100

    chore: rename miss named variables and comments

--- DocumentParser/IDocumentParser.cs
+++ DocumentParser/IDocumentParser.cs
@@ -9,6 +9,6 @@ public interface IDocumentParser
     /// Parse the input (single string) to returns a formated json.
     /// </summary>
     /// <param name="rawDocument">The input document (as string) with specific patterns (Ex. tables rows are sepparated by min 3 spaces)</param>
-    /// <returns>A formated json (key: value)</returns>
+    /// <returns>A formated json array (Ex. [value1, value2])</returns>
     string Parse(string rawDocument);
 }
