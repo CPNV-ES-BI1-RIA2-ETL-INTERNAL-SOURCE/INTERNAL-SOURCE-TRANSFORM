ae9e317da7fd87e5edb3306da63f3ae154ddd67c
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-12 09:13:16 +0100

    test: add a test for headers reminder in the DocumentParser

--- DocumentParserTests/DocumentParserTests.cs
+++ DocumentParserTests/DocumentParserTests.cs
@@ -91,4 +91,24 @@ public class DocumentParserTests
         // Then
         Assert.That(parsedDocument,Is.EqualTo("[\"Gare de Yverdon-les-Bains\",[{\"Heure de d\\u00E9part\":\"8 00\",\"Ligne\":\"\",\"Destination\":\"Lausanne\",\"Vias\":\"\",\"Voie\":\"\"},{\"Heure de d\\u00E9part\":\"\",\"Ligne\":\"IC 5\",\"Destination\":\"Gen\\u00E8ve A\\u00E9roport\",\"Vias\":\"Morges\",\"Voie\":\"2\"}],\"D\\u00E9part pour le 9 d\\u00E9cembre 2024\"]"));
     }
+    
+    [Test]
+    public void Parse_CompleteDocumentWithHeadersReminder()
+    {
+        // Given 
+        string rawDocument = "Gare de Yverdon-les-Bains\n" +
+                             " Heure de départ        Ligne    Destination         Vias                                              Voie\n" +
+                             " 8 00                   IC 5     Lausanne                                                              2\n" +
+                             " Heure de départ        Ligne    Destination         Vias                                              Voie\n" +
+                             " 16 45                  IC 5     Genève Aéroport     Morges                                            2\n" +
+                             "\n" +
+                             "\n" +
+                             "Départ pour le 9 décembre 2024\n";
+
+        // When
+        var parsedDocument = _documentParser.Parse(rawDocument);
+
+        // Then
+        Assert.That(parsedDocument,Is.EqualTo("[\"Gare de Yverdon-les-Bains\",[{\"Heure de d\\u00E9part\":\"8 00\",\"Ligne\":\"IC 5\",\"Destination\":\"Lausanne\",\"Vias\":\"\",\"Voie\":\"2\"},{\"Heure de d\\u00E9part\":\"16 45\",\"Ligne\":\"IC 5\",\"Destination\":\"Gen\\u00E8ve A\\u00E9roport\",\"Vias\":\"Morges\",\"Voie\":\"2\"}],\"D\\u00E9part pour le 9 d\\u00E9cembre 2024\"]"));
+    }
 }
