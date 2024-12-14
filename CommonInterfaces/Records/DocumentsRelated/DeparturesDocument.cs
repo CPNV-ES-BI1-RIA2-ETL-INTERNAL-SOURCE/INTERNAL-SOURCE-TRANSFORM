115a03366379035ff9c3a48cb59851389b9ccaa1
eliott <eliott.jaquier@gmail.com>
2024-12-14 17:41:25 +0100

    fix: tech doc param name was not renamed

--- CommonInterfaces/Records/DocumentsRelated/DeparturesDocument.cs
+++ CommonInterfaces/Records/DocumentsRelated/DeparturesDocument.cs
@@ -5,5 +5,5 @@
 /// </summary>
 /// <param name="RelatedTrainStationName">The train station name that the document is related to (all departures are from this station).</param>
 /// <param name="Date">The date from which the departures are listed in sentence context (Ex. 'Départ pour le 9 décembre 2024'). </param>
-/// <param name="Departure">A list of departures</param>
+/// <param name="Departures">A list of departures</param>
 public record DeparturesDocument(string RelatedTrainStationName, string Date, List<DepartureEntry> Departures);
