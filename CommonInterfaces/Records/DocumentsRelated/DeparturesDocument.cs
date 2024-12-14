5b3444ffd3f3e71f2bfcd13d6878f23b6bee7f33
eliott <eliott.jaquier@gmail.com>
2024-12-14 17:32:21 +0100

    fix: close one xml param in tech doc

--- CommonInterfaces/Records/DocumentsRelated/DeparturesDocument.cs
+++ CommonInterfaces/Records/DocumentsRelated/DeparturesDocument.cs
@@ -4,6 +4,6 @@
 /// A structure that represents a document with departures from a train station.
 /// </summary>
 /// <param name="RelatedTrainStationName">The train station name that the document is related to (all departures are from this station).</param>
-/// <param name="Date">The date from which the departures are listed in sentence context (Ex. 'Départ pour le 9 décembre 2024').
+/// <param name="Date">The date from which the departures are listed in sentence context (Ex. 'Départ pour le 9 décembre 2024'). </param>
 /// <param name="Departure">A list of departures</param>
 public record DeparturesDocument(string RelatedTrainStationName, string Date, List<DepartureEntry> Departures);
