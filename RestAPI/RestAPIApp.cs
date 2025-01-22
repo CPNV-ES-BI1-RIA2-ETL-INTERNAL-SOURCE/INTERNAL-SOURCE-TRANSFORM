513c9e1eb235774409674986f4c0f8f382241d10
eliott <eliott.jaquier@gmail.com>
2025-01-22 09:48:26 +0100

    feat: add custom OpenApiInfo

--- RestAPI/RestAPIApp.cs
+++ RestAPI/RestAPIApp.cs
@@ -2,9 +2,30 @@
 using BusinessTransformer.Records;
 using CommonInterfaces.Records.DocumentsRelated;
 using DocumentParser;
+using Microsoft.OpenApi.Models;
+
+namespace RestAPI;
 
 public class RestAPIApp
 {
+    private static readonly OpenApiInfo ApiInfo = new()
+    {
+        Title = "Document Transformation API",
+        Version = "v1",
+        Description = "An API to parse from text and transform documents into business objects based on dynamic schema mapping.",
+        Contact = new OpenApiContact
+        {
+            Name = "Eliott Jaquier, Cyprien Jaquier",
+            Email = "eliott.jaquier@eduvaud.ch",
+            Url = new Uri("https://github.com/CPNV-ES-BI1-RIA2-ETL-INTERNAL-SOURCE/INTERNAL-SOURCE-TRANSFORM/issues")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    };
+    
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +33,10 @@ public class RestAPIApp
         // Add MVC and Swagger
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", ApiInfo);
+        });
 
         // Register your dependencies (if needed)
         builder.Services.AddScoped<IStringManipulator, StandardLibStringManipulator>();
